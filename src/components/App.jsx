55ff4247942868303d6f7a2c52f8ba585c90301e
Katrina Hockman <khockman90@hotmail.com>
2019-02-14 10:36:43 -0800

    Add new route, begin state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,24 @@
-import React from "react";
-import Header from "./Header";
-import TicketList from "./TicketList";
+import React from 'react';
+import Header from './Header';
+import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import { Switch, Route } from 'react-router-dom';
 
-function App(props){
+function App(){
   let myStyledApp = {
-   backgroundColor: '#ecf0f1',
-   fontFamily: 'sans-serif',
-   paddingTop: '50px'
- }
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  };
   return (
-   <div style={myStyledApp}>
-     <Header/>
-     <TicketList/>
-   </div>
- );
+    <div style={myStyledApp}>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketControl} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
