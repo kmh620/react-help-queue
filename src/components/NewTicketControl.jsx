297eb4796f6605f077ab2bb88a49e2466e4650cf
Katrina Hockman <khockman90@hotmail.com>
2019-02-14 11:44:35 -0800

    Add confirmation questions button click function

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import ConfirmationQuestions from './ConfirmationQuestions';
+import NewTicketForm from './NewTicketForm';
+
+
 
 
 class NewTicketControl extends React.Component {
@@ -7,14 +11,25 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+   this.handleTroubleshootingConfirmation = this.handleTroubleshootingConfirmation.bind(this);
+  }
+
+  handleTroubleshootingConfirmation(){
+    this.setState({formVisibleOnPage: true});
   }
 
   render(){
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewTicketForm />;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestions onTroubleshootingConfirmation={this.handleTroubleshootingConfirmation}/>;
+    }
     return (
       <div>
-        <p>This is the NewTicketControl component!</p>
+        {currentlyVisibleContent}
       </div>
-    );
+     );
   }
 }
 
