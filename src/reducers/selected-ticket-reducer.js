5758d152cf35621e7c6efd47e8aae150a1aa6027
Student <example@example.com>
2019-02-27 16:26:51 -0800

    Add constants

--- src/reducers/selected-ticket-reducer.js
+++ src/reducers/selected-ticket-reducer.js
@@ -1,7 +1,9 @@
 
+import c from './../constants';
+
 export default (state = {}, action) => {
   switch (action.type) {
-  case 'SELECT_TICKET':
+  case c.SELECT_TICKET:
     return action.ticketId;
   default:
     return state;
